c0a7d4218b384662c09c426d0cb047a5e3e010a2
test <test>
2024-04-06 14:58:45 +0800

    update by 4.6

--- batch-request.py
+++ batch-request.py
@@ -1,68 +1,67 @@
-#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+import csv
 import sys
+import re
 import asyncio
 import aiohttp
-from bs4 import BeautifulSoup
-import xlsxwriter
+import datetime
+from tqdm import tqdm
+import async_timeout
 
 
-async def fetch_webinfo(session, url):
+async def fetch_webinfo(url):
     """
     异步获取单个URL的网页信息
     """
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
-    try:
-        async with session.get(url, headers=headers, ssl=False) as response:
-            status_code = response.status
-            html = await response.text()
-            headers = response.headers
-            soup = BeautifulSoup(html, 'html.parser')
-            title = soup.title.string.strip() if soup.title else ''
-            print(url,title)
-            return {'url': url, 'status_code': status_code, 'title': title,
-                    'Content-Length': headers.get('Content-Length',len(html) if html else 0),
-                    'Server': headers.get('Server', ''), 'headers': '\n'.join([f'{k}: {v}' for k, v in headers.items()]),
-                    'html': html, 'error': ''}
-    except aiohttp.ClientError as e:
-        print(url,e)
-        return {'url': url, 'status_code': 0, 'title': '', 'Content-Length': 0, 'Server': '',
-                'headers': '', 'html': '', 'error': str(e)}
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with async_timeout.timeout(10):
+                async with session.get(url, headers=headers, ssl=False) as response:
+                    status_code = response.status
+                    html = await response.text()
+                    headers = await response.headers
+                    titles = re.findall(
+                        '<title>(.*?)</title>', re.I | re.DOTALL)
+                    title = titles[0] if titles else ""
+                    return {'URL': url, 'Status Code': status_code, 'Title': title,
+                            'Content-Length': headers.get('Content-Length', len(html) if html else 0),
+                            'Server': headers.get('Server', ''), 'Headers': '\n'.join([f'{k}: {v}' for k, v in headers.items()]),
+                            'HTML': html, 'Error': ''}
+        except Exception as e:
+            return {'URL': url, 'Status Code': 0, 'Title': '', 'Content-Length': 0, 'Server': '',
+                    'Headers': '', 'HTML': '', 'Error': str(e)}
 
 
 async def main(urls):
     """
     主函数，处理并发获取多个URL的网页信息
     """
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for url in urls:
-            task = asyncio.ensure_future(fetch_webinfo(session, url))
-            tasks.append(task)
-        responses = await asyncio.gather(*tasks)
-        results = {url: response for url, response in zip(urls, responses)}
-        return results
+
+    tasks = [asyncio.create_task(fetch_webinfo(url)) for url in urls]
+    results = []
+
+    with tqdm(total=len(tasks)) as pbar:
+        for coro in asyncio.as_completed(tasks):
+            result = await coro
+            results.append(result)
+            pbar.update(1)
+    return results
 
 
-def save_to_xlsx(filename, results):
+def save_to_csv(filename, results):
     """
-    将网页信息保存到Excel文件中
+    将网页信息保存到csv文件中
     """
-    workbook = xlsxwriter.Workbook(filename)
-    worksheet = workbook.add_worksheet("Webpage Info")
-    worksheet.write_row(0, 0, ['URL', 'Status Code', 'Title', 'Content-Length', 'Server', 'Headers', 'HTML', 'Error'])
-    row = 1
-    for url, response in results.items():
-        worksheet.write_row(row, 0, [
-            response['url'], response['status_code'], response['title'], response['Content-Length'],
-            response['Server'], response['headers'], response['html'], response['error']
-        ])
-        row += 1
-    workbook.close()
-
+    fieldnames = ['URL', 'Status Code', 'Title',
+                  'Content-Length', 'Server', 'Headers', 'HTML', 'Error']
+    with open(filename, 'w', newline='', encoding='utf8') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(results)
 
 
 if __name__ == '__main__':
@@ -70,10 +69,12 @@ if __name__ == '__main__':
         sys.exit(f'Usage: {sys.argv[0]} urls.txt')
 
     urls_file = sys.argv[1]
-    urls = [url.strip() for url in open(urls_file, 'r', encoding='utf8').readlines()]
+    urls = [url.strip() for url in open(
+        urls_file, 'r', encoding='utf8').readlines() if url.strip()]
+    print(f'[{datetime.datetime.now()}] urls: {len(urls)}')
+    print(f'[{datetime.datetime.now()}] start')
     loop = asyncio.get_event_loop()
     results = loop.run_until_complete(main(urls))
-    save_to_xlsx(f'{urls_file}.xlsx', results)
-
-
-        
+    print(f'[{datetime.datetime.now()}] end')
+    save_to_csv(f'{urls_file}.csv', results)
+    print(f'[{datetime.datetime.now()}] save to {urls_file}.csv')
